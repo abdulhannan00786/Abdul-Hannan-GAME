8bb108f5f567bcb64e3c86f0a5e36651ad0d0c7c
abdulhannan00786 <abdulhannan10m@gmail.com>
2024-05-25 17:14:11 +0530

    added instagramwebpage.py

---
+++ instagram web page.py
@@ -0,0 +1,8 @@
+
+def create_instagram_webpage(username):
+    if username == 'abdul_hannan_0000':
+        print("Instagram webpage created successfully for user:", username)
+    else:
+        print("Invalid username. Please try again.")
+
+create_instagram_webpage('abdul_hannan_0000')
